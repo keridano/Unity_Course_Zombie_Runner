c818539e8bc3d3e6656410610e0813a4e53da9d8
Enrico <keridano@gmail.com>
2020-05-17 00:06:50 +0200

    Added weapon Zooom

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class WeaponZoom : MonoBehaviour
+{
+    private const float zoomedInMouseSensitivity = 0.5f;
+    private const float zoomedInFieldOfView = 20f;
+
+    float baseFieldOfView;
+    float baseMouseXSensitivity;
+    float baseMouseYSensitivity;
+
+    bool isZoomedIn;
+
+    RigidbodyFirstPersonController fpsController;
+
+    void Start()
+    {
+        baseFieldOfView = Camera.main.fieldOfView;
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+        baseMouseXSensitivity = fpsController.mouseLook.XSensitivity;
+        baseMouseYSensitivity = fpsController.mouseLook.YSensitivity;
+
+    }
+
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire2"))
+        {
+            if (isZoomedIn)
+            {
+                isZoomedIn = false;
+                Camera.main.fieldOfView = baseFieldOfView;
+                fpsController.mouseLook.XSensitivity = baseMouseXSensitivity;
+                fpsController.mouseLook.YSensitivity = baseMouseYSensitivity;
+            }
+            else
+            {
+                isZoomedIn = true;
+                Camera.main.fieldOfView = zoomedInFieldOfView;
+                fpsController.mouseLook.XSensitivity = zoomedInMouseSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInMouseSensitivity;
+            }
+        }
+    }
+}
